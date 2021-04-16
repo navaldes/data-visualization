6b168140d793133c28e4dcb2a890e94cb5053cc8
navaldes <Nicholas.Valdes2018@gmail.com>
2021-04-15 21:50:10 -0500

    Chapter 15 Finished

--- rw_visual.py
+++ rw_visual.py
@@ -8,7 +8,7 @@ while True:
     rw.fill_walk()
     # Plot the points in the walk
     plt.style.use('classic')
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(15,9))
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
     
